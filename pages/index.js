4fd4fd5835749dfd5fef7051e7e45af03c2d0af3
sv-rubik <tosvr@yandex.ru>
2023-02-26 11:54:38 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+// Variables
+const editBtn = document.querySelector('.profile__edit-btn')
+const popupEdit = document.getElementById('popup-edit')
+const closeBtn = document.querySelector('.popup__close-btn')
+let profileNameText = document.querySelector('.profile__name')
+let profileDescriptionText = document.querySelector('.profile__description')
+let profileNameInputField = document.getElementById('profile-name')
+let profileDescriptionInputField = document.getElementById('profile-description')
+const formElement = document.querySelector('.popup__form')
+
+// Assign current profile values to form input fields
+profileNameInputField.value = profileNameText.textContent
+profileDescriptionInputField.value = profileDescriptionText.textContent
+
+function displayNone($el) {
+  return $el.classList.remove('popup_opened')
+}
+
+function displayBlock($el) {
+  return $el.classList.add('popup_opened')
+}
+
+// Edit Form Handler - saving new values from input
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileNameText.textContent = profileNameInputField.value
+  profileDescriptionText.textContent = profileDescriptionInputField.value
+  displayNone(popupEdit)
+}
+
+// Event listeners
+editBtn.addEventListener('click', () => displayBlock(popupEdit))
+closeBtn.addEventListener('click', () => displayNone(popupEdit))
+formElement.addEventListener('submit', formSubmitHandler)
